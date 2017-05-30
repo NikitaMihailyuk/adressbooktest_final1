8d6e288628f0b9638945eff7efff708b431e9b7b
Никита <isthisnikita@gmail.com>
2017-05-30 03:10:46 +0300

    Создан базовый класс, перенесены описания методов

---
+++ adressbooktest_final/GroupRemovalTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAdressbookTests 
+{
+    [TestFixture]
+   public class GroupRemovalTests : TestBase
+    {
+
+        [Test]
+        public void GroupRemovalTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            SelectGroup(2);
+            RemoveGroup();
+            ReturnToGroupsPage();
+        }
+    }
+}
