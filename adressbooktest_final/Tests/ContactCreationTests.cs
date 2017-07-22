cd022f17e67132bd1113da62162dc597218b2543
Никита <isthisnikita@gmail.com>
2017-07-23 00:28:01 +0300

    15 ое задание

--- adressbooktest_final/Tests/ContactCreationTests.cs
+++ adressbooktest_final/Tests/ContactCreationTests.cs
@@ -4,6 +4,10 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 
 namespace WebAdressbookTests 
 {
@@ -26,8 +30,37 @@ namespace WebAdressbookTests
             }
             return contacts;
         }
+        public static IEnumerable<ContactData> ContactDataFromCsvFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            string[] lines = File.ReadAllLines(@"contacts.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                contacts.Add(new ContactData(parts[0])
+                {
+                    Lastname = parts[1],
+                    Middlename = parts[2]
+                });
 
-        [Test, TestCaseSource("RandomContactDataProvider")]
+            }
+            return contacts;
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+            List<ContactData> groups = new List<ContactData>();
+            return (List<ContactData>)
+                 new XmlSerializer(typeof(List<ContactData>))
+                 .Deserialize(new StreamReader(@"contacts.xml"));
+        }
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(
+                File.ReadAllText(@"contacts.json"));
+        }
+
+        [Test, TestCaseSource("ContactDataFromJsonFile")]
 
         public void ContactCreationTest(ContactData contact)
         {
