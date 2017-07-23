bc900db5b282bdf8553bd5f2fb599bfce0db79c2
Никита <isthisnikita@gmail.com>
2017-07-24 01:55:05 +0300

    Задание 16 из БД контакты не удаляются подходящий запрос сделать не смог.
    Структуру написал, относительно создания контакта в бд проверка выполнилась.

--- adressbooktest_final/Tests/ContactCreationTests.cs
+++ adressbooktest_final/Tests/ContactCreationTests.cs
@@ -64,14 +64,13 @@ namespace WebAdressbookTests
 
         public void ContactCreationTest(ContactData contact)
         {
-            List<ContactData> oldContacts = app.Contacts.GetContactList();
+            List<ContactData> oldContacts = ContactData.GetAll();
 
             app.Contacts.Create(contact);
 
-            Assert.AreEqual(oldContacts.Count + 1, app.Contacts.GetContactCount());
+            //Assert.AreEqual(oldContacts.Count + 1, app.Contacts.GetContactCount());
 
-
-            List<ContactData> newContacts = app.Contacts.GetContactList();
+            List<ContactData> newContacts = ContactData.GetAll();
             oldContacts.Add(contact);
             oldContacts.Sort(); 
             newContacts.Sort();
